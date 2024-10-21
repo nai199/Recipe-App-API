250db37e86f29038efc18bcb9a78e5d0c2dcbe70
Naima <163862082+nai199@users.noreply.github.com>
2024-10-21 07:28:38 -0400

    added user api code

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
+from django.contrib.auth import authenticate
 
 
 class AdminSiteTests(TestCase):
@@ -17,6 +18,8 @@ class AdminSiteTests(TestCase):
             email='naima@lexaeon.com',
             password='admin123',
         )
+
+        self.admin_user = authenticate(email='naima@lexaeon.com', password='admin123')
         self.client.force_login(self.admin_user)
 
         self.user = get_user_model().objects.create_user(
@@ -32,3 +35,16 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
